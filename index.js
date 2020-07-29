ebee7107486cc1d65f9578d8a1d93390dd87f002
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-07-29 11:08:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your Circle class here
+// Add your Circle class here// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+
+
+ get diameter(){
+   return this.radius *2
+ }
+
+ set a(newOne){
+this.radius = newOne
+ }
+ get circumference(){
+   return 2 * Math.PI * this.radius
+ }
+ get area(){
+   return Math.pow(this.radius, 2) * Math.PI;
+ }
+
+
+
+}
+ let circle= new Circle(6)
+ circle.diameter;
+ circle.a = 8
+ circle.diameter;
+
+ circle.circumference;
